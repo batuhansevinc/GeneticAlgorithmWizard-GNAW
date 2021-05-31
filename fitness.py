e6ad22eca27052b7ee239f2d10f739d12d3ccf0d
batuhansevinc <61507580+batuhansevinc@users.noreply.github.com>
2021-05-31 04:05:35 +0300

    Robby Python codes added.

---
+++ fitness.py
@@ -0,0 +1,82 @@
+import random
+def generate_individual():
+  return [random.randint(0,6) for i in range(0, 243)]
+
+#def generate_board():
+  #return [[random.randint(0,1) for i in range(0, 10)] for x in range(0, 10)]
+
+# 0 -> empty, 1 -> soda can, 2 -> wall
+# N-S-W-E-C  -> (00100)_3 -> 9
+# 0 -> do nothing, 1 -> go left, 2 -> go right, 3 -> go up, 4 -> go down
+# 5 -> pick up, 6 -> go random
+
+# testing
+# b = generate_board()
+# x, y = 0, 0
+
+def fitness(ind):
+  avg_f = 0  # average fitness
+  for a1 in range(0, 200):
+    f = 0  # fitness for current board
+    b = [[random.randint(0,1) for i in range(0, 10)] for x in range(0, 10)]
+    x, y = 0, 0   # x and y on the board, initially zero
+    for a2 in range(0, 200):
+      # find the combination:
+      # N-S-W-E-C  -> (00100)_3 -> 9
+      comb = 0
+      comb += b[x][y]  # 3^0 - just take the value
+      if x+1 == 10:  # wall
+        comb += pow(3,1) * 2   # 3^1 - wall
+      else:
+        comb += pow(3,1) * b[x][y]
+      if x-1 < 0:
+        comb += pow(3,2) * 2
+      else:
+        comb += pow(3,2) * b[x][y]
+      if y+1 == 10:
+        comb += pow(3,3) * 2
+      else:
+        comb += pow(3,3) * b[x][y]
+      if y-1 < 0:
+        comb += pow(3,4) * 2
+      else:
+        comb += pow(3,4) * b[x][y]
+      # 0 -> do nothing, 1 -> go left, 2 -> go right, 3 -> go up, 4 -> go down
+      # 5 -> pick up, 6 -> go random
+      # skipping 0 since it does nothing...
+      action = ind[comb]
+      if action == 6:
+        # go random, so set action randomly between 1 and 4
+        action = random.choice(range(1,5))  # 5 -> exclusive...
+      if action == 1:
+        # go left
+        if x-1 < 0:
+          f -= 1  # fine 1 points? update these as required...
+        else:
+          x -= 1  # update x so it moves left.
+      elif action == 2:
+        # go right
+        if x+1 == 10:
+          f -= 1
+        else:
+          x += 1
+      elif action == 3:
+        # go up
+        if y-1 < 0:
+          f -= 1
+        else:
+          y -= 1
+      elif action == 4:
+        if y+1 == 10:
+          f -= 1
+        else:
+          y += 1
+      elif action == 5:    # pick up can
+        if b[x][y] == 1:   # there is a soda can
+          b[x][y] = 0      # update board, pick it up (combination changes)
+          f += 10
+        else:
+          f -= 5
+      # repeat until 200 are done
+    avg_f += f
+  return avg_f / 200
--- main.py
+++ main.py
@@ -1,268 +1,97 @@
-import time
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QVBoxLayout
-from PyQt5.QtWidgets import*
-from PyQt5.QtCore import*
-from random import choices, randint, randrange, random
-from typing import List, Callable, Tuple
-from collections import namedtuple
-from functools import partial
+import random
+from statistics import mean
+from fitness import *
+from gui import crossover_type
 
-fitness_type = input(f"Fitness Type: Knapsack = 0, Test = 1:  ")
-selection_type = input(f"Selection Type: Elitism = 0, Roulette = 1, Rank = 2, Tournament = 3 : ")
-crossover_type = input(f"Crossover Type: Single = 0, Multi = 1, Uniform = 2: ")
-mutation_type = input(f"Mutation Type: Random Flip = 0, Test = 1:  ")
 
+crossover_type = "0"
+import os
 
-value_population_limit = 10 #int(input("Enter Population Size: "))  # 10
-value_generation_limit = 100 #int(input("Enter Generation Limit: "))  # 100
-value_fitness_limit = 1000 #int(input("Enter Fitness Score Limit(0-1410): "))  # 1310
-value_weight_limit = 750 #int(input("Enter Max Weight(25-3648): "))  # 3000
 
-Genome = List[int]
-Population = List[Genome]
-FitnessFunc = Callable[[Genome], int]
-PopulateFunc = Callable[[], Population]
-SelectionFunc = Callable[[Population, FitnessFunc], Tuple[Genome, Genome]]
-CrossoverFunc = Callable[[Genome, Genome], Tuple[Genome, Genome]]
-MutationFunc = Callable[[Genome], Genome]
-PrinterFunc = Callable[[Population, int, FitnessFunc], None]
-Thing = namedtuple('Thing', ['name', 'value', 'weight'])
 
+mutation_val = 0.1 #float(input(f"Set Mutation Probablity: "))
 
 
+def generate_individual():
 
-things = [
-    Thing('Laptop', 500, 2200),
-    Thing('Headphones', 150, 160),
-    Thing('Coffee Mug', 60, 350),
-    Thing('Notepad', 40, 333),
-    Thing('Water Bottle', 30, 192),
-]
+    return [random.randint(0, 6) for i in range(0, 243)]
 
-more_things = [
-                  Thing('Mints', 5, 25),
-                  Thing('Socks', 10, 38),
-                  Thing('Tissues', 15, 80),
-                  Thing('Phone', 500, 200),
-                  Thing('Baseball Cap', 100, 70)
-              ] + things
 
+def mutation(ind, m):
+    for k, v in enumerate(ind):
+        if random.random() < m:
+            ind[k] = random.randint(0, 6)
+    return ind
 
-def generate_things(num: int) -> [Thing]:
-    return [Thing(f"thing{i}", i, i) for i in range(1, num + 1)]
 
+def crossover_singlepoint(ind1, ind2):
+    # print(f"111111")
+    p = random.randint(0, len(ind1))
+    c1 = ind1[0: p] + ind2[p:]
+    c2 = ind2[0: p] + ind1[p:]
+    return c1, c2
 
-def generate_genome(length: int) -> Genome:
-    return choices([0, 1], k=length)
 
+def crossover_multipoint(ind1, ind2):
+    # print(f"222222")
+    p3 = random.randint(0, len(ind1))
+    p4 = random.randint(0, len(ind1) - 1)
+    c1 = ind1[p3: p4] + ind2[p3:p4]
+    c2 = ind2[p4: p3] + ind1[p4:p3]
 
-def generate_population(size: int, genome_length: int) -> Population:
-    return [generate_genome(genome_length) for _ in range(size)]
+    return c1, c2
 
 
-def fitness(genome: Genome, things: [Thing], weight_limit: int) -> int:
-    #print(f"fitness")
-    if len(genome) != len(things):
-        raise ValueError("genome and things must be of same length")
 
-    weight = 0
-    value = 0
-    for i, thing in enumerate(things):
-        if genome[i] == 1:
-            weight += thing.weight
-            value += thing.value
+class bam:
 
-            if weight > weight_limit:
-                return 0
+      def __init__(self):
+            super().__init__()
 
-    return value
+            self.p = None
 
-def fitness1(genome: Genome, things: [Thing], weight_limit: int) -> int:
-    #print(f"fitness1")
-    if len(genome) != len(things):
-        raise ValueError("genome and things must be of same length")
+      def start_f(self):
+        # genetic algorithm:
 
-    weight = 0
-    value = 0
-    for i, thing in enumerate(things):
-        if genome[i] == 1:
-            weight += thing.weight
-            value += thing.value
+        N = 100 #input(int(f"number of individuals in population"))  # number of individuals in population 100
+        G = 200 #input(int(f"umber of generations"))  # number of generations 200
+        pop = list()
+        # init pop
+        for i in range(N):
+            pop.append(generate_individual())
 
-            if weight > weight_limit:
-                return 0
+        dispatcher = {'0': crossover_singlepoint, '1': crossover_multipoint}
+        # genetic algorithm loop
+        for i in range(G):
+            f_vals = list()  # a place for fitness values of each individual
+            for ind in pop:
+                f_vals.append(fitness(ind))  # compute fitness for all in the population
+            newpop = list()  # the NEXT generation...
+            bfitpop = max(f_vals)
+            while len(newpop) < N:  # keep doing this until we have N number of inds in newpop
 
-    return value
+                idx = random.choices(range(0, len(pop)), k=16)  # -> randomly selects 16 inds
+                val = [f_vals[p] for p in idx]  # -> grabs the fitness values for selected idx
+                p1 = pop[idx[val.index(max(val))]]
+                # do it again
+                # burada aynı idx geliyor olmasın? üstteki listeden iki tane en iyi seçmeyi deneyelim
+                idx.pop(val.index(max(val)))  # remove max
+                val.pop(val.index(max(val)))
 
-def elitism(population: Population, fitness_func: FitnessFunc) -> Population:
-    return choices(
-        population=population,
-        weights=[fitness_func(genome) for genome in population],
-        k=2
+                p2 = pop[idx[val.index(max(val))]]
 
-    )
+                m = mutation_val
+                # children
+                c1 = list()
+                c2 = list()
+                c1, c2 = dispatcher[crossover_type](p1, p2)
+                c1 = mutation(c1, m)
+                c2 = mutation(c2, m)
+                newpop.append(c1)
+                newpop.append(c2)
+            pop = newpop
+            print("mean: " + str(mean(f_vals)) + ", best: " + str(bfitpop))
 
 
-def tournament(population: Population, fitness_func: FitnessFunc) -> Population:
-    return choices(
-        population=population,
-        weights=[fitness_func(genome) for genome in population],
-        k=2
 
-    )
 
-
-def single_point_crossover(a: Genome, b: Genome) -> Tuple[Genome, Genome]:
-    if len(a) != len(b):
-        raise ValueError("Genomes a and b must be of same length")
-
-    length = len(a)
-    if length < 2:
-        return a, b
-    #print(f"This is single")
-    p = randint(1, length - 1)
-
-    return a[0:p] + b[p:], b[0:p] + a[p:]
-
-
-def multi_point_crossover(a: Genome, b: Genome) -> Tuple[Genome, Genome]:
-    if len(a) != len(b):
-        raise ValueError("Genomes a and b must be of same length")
-    #print(f"This is multi")
-    length = len(a)
-    if length < 2:
-        return a, b
-
-    p1 = randint(1, length)
-    p2 = randint(1, length - 1)
-    if p2 >= p1:
-        p2 += 1
-
-    else:
-        p1, p2 = p2, p1
-
-    a[p1:p2], b[p1:p2] \
-        = b[p1:p2], a[p1:p2]
-
-    return a, b
-
-
-def uniform_crossover(a: Genome, b: Genome, abpb: Genome, ) -> Tuple[Genome, Genome, Genome]:
-    if len(a) != len(b):
-        raise ValueError("Genomes a and b must be of same length")
-
-    length = len(a)
-    if length < 2:
-        return a, b, abpb
-
-    for i in randrange(length):
-        if random() < abpb:
-            a[i], b[i] = b[i], a[i]
-
-    return a, b, abpb
-
-
-def mutation(genome: Genome, num: int = 1, probability: float = 0.5) -> Genome:
-
-    for _ in range(num):
-        index = randrange(len(genome))
-        genome[index] = genome[index] if random() > probability else abs(genome[index] - 1)
-        #print(f"mutation")
-    return genome
-
-def mutation1(genome: Genome, num: int = 1, probability: float = 0.5) -> Genome:
-
-    for _ in range(num):
-        index = randrange(len(genome))
-        genome[index] = genome[index] if random() > probability else abs(genome[index] - 1)
-        #print(f"mutation1")
-    return genome
-
-
-dispatcher = {'0': single_point_crossover,'1': multi_point_crossover,'2': uniform_crossover}
-dispatcher2 = {'0': mutation,'1': mutation1}
-dispatcher3 = {'0': elitism,'1': tournament}
-
-
-def run_evolution(
-        populate_func: PopulateFunc,
-        fitness_func: FitnessFunc,
-        fitness_limit: int = value_fitness_limit,
-        selection_func: SelectionFunc = dispatcher3[selection_type],
-        crossover_func: CrossoverFunc = dispatcher[crossover_type],
-        mutation_func: MutationFunc = dispatcher2[mutation_type],
-        generation_limit: int = value_generation_limit,
-
-) -> Tuple[Population, int]:
-    population = populate_func()
-
-
-
-    for i in range(generation_limit):
-        population = sorted(population, key=lambda genome: fitness_func(genome), reverse=True)
-
-        if fitness_func(population[0]) >= fitness_limit:
-            break
-
-
-        next_generation = population[0:2]
-
-        for j in range(int(len(population) / 2) - 1):
-            parents = selection_func(population, fitness_func)
-            offspring_a, offspring_b = crossover_func(parents[0], parents[1])
-            offspring_a = mutation_func(offspring_a)
-            offspring_b = mutation_func(offspring_b)
-            next_generation += [offspring_a, offspring_b]
-
-        population = next_generation
-
-        population = sorted(population, key=lambda genome: fitness_func(genome), reverse=True)
-
-    return population, i
-
-dispatcher1 = {'0': fitness,'1': fitness1}
-start = time.time()
-
-population, generations = run_evolution(
-    populate_func=partial(
-        generate_population, size=value_population_limit, genome_length=len(more_things)
-    ),
-    fitness_func=partial(
-        dispatcher1[fitness_type], things=more_things, weight_limit=value_weight_limit
-    ),
-    fitness_limit=value_fitness_limit,
-    generation_limit=value_generation_limit
-)
-end = time.time()
-
-
-def genome_to_things(genome: Genome, things: [Thing]) -> [Thing]:
-    result = []
-    total_fitness_score = 0
-    total_weight = 0
-
-    for i, thing in enumerate(things):
-        if genome[i] == 1:
-            result += [thing.name]
-            total_fitness_score += thing.value
-            total_weight += thing.weight
-
-    success_rate = total_fitness_score * 100 / value_fitness_limit
-    load_rate = total_weight * 100 / value_weight_limit
-
-    print(f"Fitness Score: {total_fitness_score}")
-    print(f"Success Rate %{success_rate}")
-    print(f"Total Weight: {total_weight}")
-    print(f"Load Rate % {load_rate} ")
-
-    return result
-
-
-print(f"Number of Generations: {generations}")
-print(f"Total Running Time: {end - start}s")
-print(f"Best Solution: {genome_to_things(population[0], more_things)}")
-
-
-
-
-myfunct(main.all)
